635ac034043a9a9aa6c17a1b88d403abed5a1ecb
Thiago Goncalves <thiagodelimag@gmail.com>
2022-04-13 14:55:24 -0300

    first commit

---
+++ src/main/kotlin/br/com/bondevendas/pixcopypaste/constants/IDPixPayloadQrCode.kt
@@ -0,0 +1,19 @@
+package br.com.bondevendas.pixcopypaste.constants
+
+object IDPixPayloadQrCode {
+    const val ID_PAYLOAD_FORMAT_INDICATOR = "00"
+    const val ID_MERCHANT_ACCOUNT_INFORMATION = "26"
+    const val ID_MERCHANT_ACCOUNT_INFORMATION_GUI = "00"
+    const val ID_MERCHANT_ACCOUNT_INFORMATION_KEY = "01"
+    const val ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION = "02"
+    const val ID_MERCHANT_CATEGORY_CODE = "52"
+    const val ID_TRANSACTION_CURRENCY = "53"
+    const val ID_TRANSACTION_AMOUNT = "54"
+    const val ID_COUNTRY_CODE = "58"
+    const val ID_MERCHANT_NAME = "59"
+    const val ID_MERCHANT_CITY = "60"
+    const val ID_ADDITIONAL_DATA_FIELD_TEMPLATE = "62"
+    const val ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID = "05"
+    const val ID_CRC16 = "6304"
+
+}
